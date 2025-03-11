c77fed3ee69ecef1578ce1a9fd28427dde0507a7
amithaisuru <rkamithaisuru@gmail.com>
2025-03-11 15:53:52 +0530

    Revert "remove unused files"
    This reverts commit 38d9bff01ca020fbbccfdbb561e5514b72c36ec4.

--- pages/trending.py
+++ pages/trending.py
@@ -1,6 +1,33 @@
 # pages/trending.py
 import streamlit as st
 
+from database import get_db_connection, get_dict_cursor_connection
 
-def display_trending():
-    pass
+
+def get_trending_books():
+    conn, cur = get_dict_cursor_connection()
+    try:
+        cur.execute('''
+            SELECT * FROM books
+            ORDER BY (rating_count * average_rating) DESC
+            LIMIT 10
+        ''')
+        return cur.fetchall()
+    finally:
+        cur.close()
+        conn.close()
+
+def trending_page():
+    st.title("Trending Books")
+    
+    trending_books = get_trending_books()
+    
+    for book in trending_books:
+        col1, col2 = st.columns([1, 3])
+        with col1:
+            if book['cover_image_url']:
+                st.image(book['cover_image_url'], width=100)
+        with col2:
+            st.subheader(book['title'])
+            st.write(f"Authors: {', '.join(book['authors'])}")
+            st.write(f"Rating: {book['average_rating']:.2f} ({book['rating_count']} ratings)")
